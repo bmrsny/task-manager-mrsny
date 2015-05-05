5f2e18c4bb07f84f28975646ed13632d59ac399e
Brandon Mrsny <bmrsny@gmail.com>
2015-05-05 09:15:18 -0600

    fixed validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :list
   validates :title, presence: true
+  validates :description, presence: true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   it { should validate_presence_of(:title) }
+
+  it { should validate_presence_of(:description) }
+
   it { should belong_to(:list)}
 end
