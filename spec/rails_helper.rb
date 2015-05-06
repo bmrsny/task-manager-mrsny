b390b47e6a673f468a083b0cd01e4a0dccefabd7
Brandon Mrsny <bmrsny@gmail.com>
2015-05-06 06:12:13 -0600

    remove simple cov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,3 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter "/spec/"
-  add_filter "/config."
-end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
